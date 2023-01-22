10734ad933ebf59f9140d653327842d6ed15b404
Victor Motta <victor.mottag@gmail.com>
2023-01-22 14:42:18 -0300

    Projeto finalizado, sem bônus

--- src/pages/NovaEntrada/NovaEntrada.js
+++ src/pages/NovaEntrada/NovaEntrada.js
@@ -6,17 +6,57 @@ import {
     StyledButtonSendNovaEntrada,
     StyledButtonCancelNovaEntrada,
 } from "./styled";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/auth";
+import axios from "axios";
+import { BaseUrl } from "../../constants/urls";
 
 const NovaEntrada = () => {
+    const { user } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [value, setValue] = useState();
+    const [description, setDescription] = useState();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const body = {
+            description,
+            value,
+            type: "entrada",
+        };
+        const config = {
+            headers: {
+                Authorization: `Bearer ${user.token}`,
+            },
+        };
+
+        const promisse = axios.post(`${BaseUrl}/registers`, body, config);
+        promisse.then((res) => {
+            console.log(res.data);
+            navigate("/home");
+        });
+        promisse.catch((err) => {
+            alert(err.response.data);
+        });
+    };
 
     return (
         <StyledMainContainerNovaEntrada>
             <StyledSecondaryContainerNovaEntrada>
                 <h1>Nova entrada</h1>
-                <StyledContainerFormNovaEntrada>
-                    <input type='number' placeholder='Valor' />
-                    <input type='text' placeholder='Descrição' />
+                <StyledContainerFormNovaEntrada onSubmit={handleSubmit}>
+                    <input
+                        type='number'
+                        placeholder='Valor'
+                        onChange={(e) => setValue(e.target.value)}
+                        value={value}
+                    />
+                    <input
+                        type='text'
+                        placeholder='Descrição'
+                        onChange={(e) => setDescription(e.target.value)}
+                        value={description}
+                    />
                     <StyledButtonSendNovaEntrada>Salvar entrada</StyledButtonSendNovaEntrada>
                 </StyledContainerFormNovaEntrada>
                 <StyledButtonCancelNovaEntrada onClick={() => navigate("/home")}>
--- src/pages/NovaSaida/NovaSaida.js
+++ src/pages/NovaSaida/NovaSaida.js
@@ -6,17 +6,57 @@ import {
     StyledButtonSendNovaSaida,
     StyledButtonCancelNovaSaida,
 } from "./styled";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/auth";
+import axios from "axios";
+import { BaseUrl } from "../../constants/urls";
 
 const NovaSaida = () => {
+    const { user } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [value, setValue] = useState();
+    const [description, setDescription] = useState();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const body = {
+            description,
+            value,
+            type: "saida",
+        };
+        const config = {
+            headers: {
+                Authorization: `Bearer ${user.token}`,
+            },
+        };
+
+        const promisse = axios.post(`${BaseUrl}/registers`, body, config);
+        promisse.then((res) => {
+            console.log(res.data);
+            navigate("/home");
+        });
+        promisse.catch((err) => {
+            alert(err.response.data);
+        });
+    };
 
     return (
         <StyledMainContainerNovaSaida>
             <StyledSecondaryContainerNovaSaida>
                 <h1>Nova saída</h1>
-                <StyledContainerFormNovaSaida>
-                    <input type='number' placeholder='Valor' />
-                    <input type='text' placeholder='Descrição' />
+                <StyledContainerFormNovaSaida onSubmit={handleSubmit}>
+                    <input
+                        type='number'
+                        placeholder='Valor'
+                        onChange={(e) => setValue(e.target.value)}
+                        value={value}
+                    />
+                    <input
+                        type='text'
+                        placeholder='Descrição'
+                        onChange={(e) => setDescription(e.target.value)}
+                        value={description}
+                    />
                     <StyledButtonSendNovaSaida>Salvar saída</StyledButtonSendNovaSaida>
                 </StyledContainerFormNovaSaida>
                 <StyledButtonCancelNovaSaida onClick={() => navigate("/home")}>
